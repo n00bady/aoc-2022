b5f65ab6452b7e8f45e32f3b61ae4da510718a26
17ms <79069176+17ms@users.noreply.github.com>
2022-12-02 19:02:33 +0200

    day 2

---
+++ day02/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func solve() (int, int) {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	totalA := 0
+	totalB := 0
+
+	combinations := map[string][2]int{
+		"A X": {4, 3},
+		"B X": {1, 1},
+		"C X": {7, 2},
+		"A Y": {8, 4},
+		"B Y": {5, 5},
+		"C Y": {2, 6},
+		"A Z": {3, 8},
+		"B Z": {9, 9},
+		"C Z": {6, 7},
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		totalA += combinations[line][0]
+		totalB += combinations[line][1]
+	}
+
+	return totalA, totalB
+}
+
+func main() {
+	result_a, result_b := solve()
+	fmt.Printf("A: %d\nB: %d\n", result_a, result_b)
+}

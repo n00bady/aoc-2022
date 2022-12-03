fd516ec5af804cfc1811c1a81a06949afe5d2dcf
17ms <79069176+17ms@users.noreply.github.com>
2022-12-03 21:38:23 +0200

    day 3

--- README.md
+++
@@ -1,3 +0,0 @@
-<div align="center">
-    <span><img src="https://github.com/17ms/aoc-2022/blob/master/docs/gopher.png" width="600"></span>
-</div>
---
+++ day03/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func solve() (int, int) {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	totalA := 0
+	totalB := 0
+	counter := 1
+	var topThree []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		topThree = append(topThree, line)
+		compartment1 := line[:len(line)/2]
+		compartment2 := line[len(line)/2:]
+
+		if counter%3 == 0 && counter != 0 {
+			commonB := make(map[rune]bool)
+
+			for _, char := range topThree[0] {
+				if strings.Contains(topThree[1], string(char)) && strings.Contains(topThree[2], string(char)) && !commonB[char] {
+					commonB[char] = true
+
+					if int(char)-64 > 26 {
+						totalB += int(char) - 64 - 32
+					} else {
+						totalB += int(char) - 64 + 26
+					}
+
+					break
+				}
+			}
+
+			topThree = topThree[:0]
+		}
+
+		commonA := make(map[rune]bool)
+
+		for _, char := range compartment1 {
+			if strings.Contains(compartment2, string(char)) && !commonA[char] {
+				commonA[char] = true
+
+				if int(char)-64 > 26 {
+					totalA += int(char) - 64 - 32
+				} else {
+					totalA += int(char) - 64 + 26
+				}
+			}
+		}
+
+		counter++
+	}
+
+	return totalA, totalB
+}
+
+func main() {
+	result_a, result_b := solve()
+	fmt.Printf("A: %d\nB: %d\n", result_a, result_b)
+}

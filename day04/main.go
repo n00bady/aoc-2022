60991cc0a5dfd526b5e38094144aa318461a056a
17ms <79069176+17ms@users.noreply.github.com>
2022-12-05 08:17:57 +0200

    day 4

---
+++ day04/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func solve() (int, int) {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	totalA := 0
+	totalB := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		var a1, a2, b1, b2 int
+		fmt.Sscanf(line, "%d-%d,%d-%d", &a1, &a2, &b1, &b2)
+
+		if (a1 <= b1 && a2 >= b2) || (a1 >= b1 && a2 <= b2) {
+			totalA++
+			totalB++
+		} else if (a1 >= b1 && a1 <= b2) || (b1 >= a1 && b1 <= a2) {
+			totalB++
+		}
+	}
+
+	return totalA, totalB
+}
+
+func main() {
+	result_a, result_b := solve()
+	fmt.Printf("A: %d\nB: %d\n", result_a, result_b)
+}
